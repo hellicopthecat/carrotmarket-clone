ec20c982b9f97830f8e408c1ea42730d0675079a
Hellicat <jhccop@naver.com>
2023-11-07 23:11:50 +0900

    login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
+"use client";
+import Enter from "./main/page";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="text-red-600">hi</h1>
-      <details>
-        <summary className="select-none cursor-pointer transition ease-in-out duration-300">
-          hohoho
-        </summary>
-        <span className="selection:bg-indigo-500">hohhohaohoahao</span>
-      </details>
+      <Enter />
     </main>
   );
 }
